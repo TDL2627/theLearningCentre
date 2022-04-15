ddfacb9c7bbfd702235f70bb4eb8b4f5acf4c8bd
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-15 17:09:03 +0200

    update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,6 +5,15 @@
    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">ABOUT</h1>
 <div class="content text-start">
 <div class="container-fluid">
+  <p>
+    The Learning Centre was established in 2019 as a learning platform to cater for individual learning needs.
+    The Learning Centre is an independent small group focused,special needs learning platform that promotes expediting the full potential of learners
+    through the provision of individually specially designed working programs to suit the learner's needs allowing them to become who they meant to be.
+    Boasting a staff of highly skilled and caring educators to ensure your child receives the specific attention they need.At The Learning Centre,
+     we follow the integration of the ABA curriculum and WCED special needs curriculum which are further adapted to accommodate the need of the learner.
+     Our home away from home.A fully equipped learning environment that provides a relaxed atmosphere conductive to full potential learning.
+
+  </p>
 <h3 class="subhead">WHAT WE PROVIDE :</h3>
 <div class="row">
 <div class="col-lg-6">
@@ -13,10 +22,13 @@
       Safe and secure premises
     </li>
     <li  data-icon="💙">
-       Modern spacious learning space
+       Modern spacious learning loving environment
     </li>
     <li  data-icon="💙">
       Affordable fees
+    </li>
+    <li  data-icon="💙">
+     We apply speech therapy and occupational therapy 
     </li>
     <li  data-icon="💙">
       Qualified facilitators
@@ -39,6 +51,9 @@
     </li>
     <li  data-icon="💙">
       Pecs enhanced activities if required
+    </li>
+    <li  data-icon="💙">
+      Skills development programs eg baking, jewellery and many more
     </li>
     <li  data-icon="💙">
      Small groups to accomadate individual learning
