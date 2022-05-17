f8efd62bf5141a97ae2f4b346eee6808e3540a24
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2022-05-17 13:16:09 +0200

    upload

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,85 +1,88 @@
 <template>
   <div id="testimonials">
- <div class="relative bg-white overflow-hidden">
-    <div class="max-w-7xl mx-auto text-center">
-   <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mainhead" data-aos="zoom-in">TESTIMONIALS</h1>
+    <div class="relative bg-white overflow-hidden">
+      <div class="max-w-7xl mx-auto text-center">
+        <h1
+          class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mainhead"
+          data-aos="zoom-in"
+        >
+          TESTIMONIALS
+        </h1>
 
-<div class="container">
-<div class="row">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="card" style="width: 18rem" data-aos="fade-up-right">
+                <div class="card-body">
+                  <p class="card-text">
+                    "Since my daughter started at The Learning Centre in October
+                    2020, she has improved in leaps and bounds. Her
+                    communication has improved, her interest in school work has
+                    peaked and she has become more social with her peers."
+                  </p>
+                  <h5 class="card-title">Bahijah</h5>
+                </div>
+              </div>
+            </div>
 
-<div class="col-lg-4">
-<div class="card" style="width: 18rem;"  data-aos="fade-up-right">
-  <div class="card-body">
-    <p class="card-text">"Since my daughter started at The Learning Centre in October 2020, she has improved in leaps and bounds. Her communication has improved, her interest in school work has peaked and she has become more social with her peers."</p>
-    <h5 class="card-title">Bahijah</h5>
-  </div>
-</div>
-</div>
+            <div class="col-lg-4">
+              <div class="card" style="width: 18rem" data-aos="fade-up-left">
+                <div class="card-body">
+                  <p class="card-text">
+                    "We were introduced to Fazlin (The Learning Centre) towards
+                    the end of 2016 by Carol a representative of SNAP (Special
+                    Needs Adapted Program) from Durbanville at our home.
+                    Fazlin's tutoring of Timothy greatly impacted our lives as a
+                    family and we have never looked back. Timothy was taught to
+                    understand language although he cannot speak. He clearly
+                    understands what is said to him most of the time. Life
+                    became a lot easier for us to communicate with Timothy and
+                    he uses his hand gestures to communicate with us. The
+                    progress of Timothy can only be attributed to the dedicated
+                    staff/team of the Learning Centre!"
+                  </p>
+                  <h5 class="card-title">Ivan, Karen and Timothy</h5>
+                </div>
+              </div>
+            </div>
 
-<div class="col-lg-4">
-<div class="card" style="width: 18rem;"  data-aos="fade-up-left">
-  <div class="card-body">
-    <p class="card-text">"We were introduced to Fazlin (The Learning Centre) towards the end of 2016 by Carol a representative of SNAP (Special Needs Adapted Program) from Durbanville at our home.
-
-Fazlin's tutoring of Timothy greatly impacted our lives as a family and we have never looked back.
-
-Timothy was taught to understand language although he cannot speak. He clearly understands what is said to him most of the time.
-
-Life became a lot easier for us to communicate with Timothy and he uses his hand gestures to communicate with us.
-
-The progress of Timothy can only be attributed to the dedicated staff/team of the Learning Centre!"</p>
-    <h5 class="card-title">Ivan, Karen and Timothy</h5>
-  </div>
-</div>
-</div>
-
-<div class="col-lg-4">
-<div class="card" style="width: 18rem;"  data-aos="fade-up-right">
-  <div class="card-body">
-    <p class="card-text">"A "</p>
-    <h5 class="card-title">Kasifa Adams</h5>
-  </div>
-</div>
-</div>
-
-
-</div>
-</div>
-
-
-
+            <div class="col-lg-4">
+              <div class="card" style="width: 18rem" data-aos="fade-up-right">
+                <div class="card-body">
+                  <p class="card-text">"A testimonials"</p>
+                  <h5 class="card-title">Kasifa Adams</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
- </div>
-
   </div>
 </template>
 
 <script>
-export default {
-
-
-}
+export default {};
 </script>
 
 <style scoped>
-.card{
-background: rgba( 0, 90, 255, 0.25 );
-box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-backdrop-filter: blur( 4px );
--webkit-backdrop-filter: blur( 4px );
-border-radius: 10px;
-border: 1px solid rgba( 255, 255, 255, 0.18 );
-margin-top: 20px;
-margin-bottom: 20px;
-
+.card {
+  background: rgba(0, 90, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
-.card-text{
-  overflow-y:scroll;
+.card-text {
+  overflow-y: scroll;
   height: 100px;
 }
-@media only screen and (max-width:700px) {
-   .card{
-       margin-left: 10%;
-   } 
+@media only screen and (max-width: 700px) {
+  .card {
+    margin-left: 10%;
+  }
 }
 </style>
